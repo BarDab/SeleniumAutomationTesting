b4fc711d31fad633ca7cfd0f1b73ca05af1b69be
BarDab <barpl7@gmail.com>
2021-02-01 21:02:21 +0100

    web-driver-6

---
+++ src/test/java/com/bardab/SeleniumHubTest.java
@@ -0,0 +1,66 @@
+package com.bardab;
+
+import static org.testng.Assert.assertEquals;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
+
+public class SeleniumHubTest {
+	
+	
+	
+	
+//	@Test
+//	public void standalone() throws MalformedURLException, InterruptedException {
+//		DesiredCapabilities capabilities = new DesiredCapabilities();
+//		capabilities.setBrowserName("chrome");
+//		
+//		
+//		WebDriver remoteDriver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"),capabilities);
+//		remoteDriver.get("https://www.facebook.com/");
+//	
+//		assertEquals(remoteDriver.findElement(By.id("consent_cookies_title")).getText(),"Accept cookies from Facebook on this browser?");
+//		Thread.sleep(2000);
+//		remoteDriver.close();
+//		
+//	}
+	
+	@Test(threadPoolSize=2, invocationCount=4)
+	public void inChrome() throws MalformedURLException, InterruptedException {
+		DesiredCapabilities capabilities = new DesiredCapabilities();
+		capabilities.setBrowserName("chrome");
+		
+		
+		WebDriver remoteDriver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"),capabilities);
+		remoteDriver.get("https://www.facebook.com/");
+	
+		assertEquals(remoteDriver.findElement(By.id("consent_cookies_title")).getText(),"Accept cookies from Facebook on this browser?");
+		Thread.sleep(2000);
+		remoteDriver.quit();
+		
+	}
+	
+	@Test(threadPoolSize=2, invocationCount=4)
+	public void inFirefox() throws MalformedURLException, InterruptedException {
+		DesiredCapabilities capabilities = new DesiredCapabilities();
+		capabilities.setBrowserName("firefox");
+		
+		
+		WebDriver remoteDriver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"),capabilities);
+		remoteDriver.get("https://www.facebook.com/");
+	
+		assertEquals(remoteDriver.findElement(By.id("consent_cookies_title")).getText(),"Accept cookies from Facebook on this browser?");
+		Thread.sleep(2000);
+		remoteDriver.quit();
+		
+	}
+	
+	
+
+}
